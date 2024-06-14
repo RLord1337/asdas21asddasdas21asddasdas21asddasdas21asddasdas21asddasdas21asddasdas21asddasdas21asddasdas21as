4df3d610a30e3183c8b7c89a23e57fb7badb4780
Lord <106379997+RLord1337@users.noreply.github.com>
2024-06-14 23:39:18 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,26 +1,14 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 
-if not speaker then
-    error("No speaker found")
-end
-
 local decoder = dfpwm.make_decoder()
-local file = fs.open("VillagerNLoud.dfpwm", "rb")
-
-if not file then
-    error("Failed to open file")
-end
-
 while true do
-    local chunk = file.read(16 * 1024)
-    if not chunk then break end
 
-    local buffer = decoder(chunk)
-
-    while not speaker.playAudio(buffer) do
-        os.pullEvent("speaker_audio_empty")
+    for chunk in io.lines("VillagerNLoud.dfpwm", 16 * 1024) do
+        local buffer = decoder(chunk)
+    
+        while not speaker.playAudio(buffer) do
+            os.pullEvent("speaker_audio_empty")
+        end
     end
 end
-
-file.close()
