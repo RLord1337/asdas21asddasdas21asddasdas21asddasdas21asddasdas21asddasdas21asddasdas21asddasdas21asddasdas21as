47a747e466b73e2fd47293243d88c116ef5cfc13
Lord <106379997+RLord1337@users.noreply.github.com>
2024-06-15 13:04:55 +0200

    Update 1main.lua

--- 1main.lua
+++ 1main.lua
@@ -3,6 +3,7 @@ local speakers = {peripheral.find("speaker")}
 local decoder = dfpwm.make_decoder()
 local running = false
 local currentPlayback
+local queue = {}
 
 if #speakers == 0 then
     print("No speakers found")
@@ -47,6 +48,13 @@ local function playFile(filename)
     running = false
 end
 
+local function stopPlayback()
+    running = false
+    if currentPlayback then
+        currentPlayback.terminate()
+    end
+end
+
 local function drawGUI()
     term.clear()
     term.setCursorPos(1, 1)
@@ -58,26 +66,38 @@ local function drawGUI()
         print(i .. ". " .. file)
     end
     print("------------")
-    print("Enter the number of the file to play, or 'stop' to stop playback:")
+    print("Queue:")
+    for i, file in ipairs(queue) do
+        print(i .. ". " .. file)
+    end
+    print("------------")
+    print("[Play] [Stop] [Add to Queue] [Clear Queue]")
 end
 
 local function handleUserInput()
     while true do
         drawGUI()
-        local input = read()
-        if input == "stop" then
-            running = false
-            if currentPlayback then
-                currentPlayback.terminate()
-            end
-        else
-            local fileIndex = tonumber(input)
-            if fileIndex and fileIndex > 0 and fileIndex <= #listFiles() then
-                local filename = listFiles()[fileIndex]
-                if currentPlayback then
-                    currentPlayback.terminate()
+        local event, button, x, y = os.pullEvent("mouse_click")
+        if y == 18 then
+            if x >= 1 and x <= 5 then
+                if #queue > 0 then
+                    local filename = table.remove(queue, 1)
+                    if currentPlayback then
+                        currentPlayback.terminate()
+                    end
+                    currentPlayback = parallel.waitForAny(function() playFile(filename) end)
                 end
-                currentPlayback = parallel.waitForAny(function() playFile(filename) end)
+            elseif x >= 7 and x <= 11 then
+                stopPlayback()
+            elseif x >= 13 and x <= 23 then
+                print("Enter file number to add to queue:")
+                local input = read()
+                local fileIndex = tonumber(input)
+                if fileIndex and fileIndex > 0 and fileIndex <= #listFiles() then
+                    table.insert(queue, listFiles()[fileIndex])
+                end
+            elseif x >= 25 and x <= 35 then
+                queue = {}
             end
         end
     end
