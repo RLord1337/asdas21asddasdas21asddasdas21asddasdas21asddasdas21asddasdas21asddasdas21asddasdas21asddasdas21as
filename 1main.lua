2422741ffc7de057ae50b5b9eb442c8e7c2c82ee
Lord <106379997+RLord1337@users.noreply.github.com>
2024-06-15 13:03:05 +0200

    Update 1main.lua

--- 1main.lua
+++ 1main.lua
@@ -1,32 +1,86 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speakers = {peripheral.find("speaker")}
 local decoder = dfpwm.make_decoder()
+local running = false
+local currentPlayback
 
 if #speakers == 0 then
     print("No speakers found")
     return
 end
 
-local file = fs.open("club.dfpwm", "rb")
-if not file then
-    print("Audio file not found")
-    return
+local function listFiles()
+    local files = fs.list("")
+    local dfpwmFiles = {}
+    for _, file in ipairs(files) do
+        if file:sub(-6) == ".dfpwm" then
+            table.insert(dfpwmFiles, file)
+        end
+    end
+    return dfpwmFiles
 end
 
-while true do
-    local chunk = file.read(16 * 1024)
-    if not chunk then
-        break
+local function playFile(filename)
+    local file = fs.open(filename, "rb")
+    if not file then
+        print("Audio file not found")
+        return
     end
 
-    local buffer = decoder(chunk)
-    
-    for _, speaker in pairs(speakers) do
-        while not speaker.playAudio(buffer) do
-            os.pullEvent("speaker_audio_empty")
+    running = true
+    while running do
+        local chunk = file.read(16 * 1024)
+        if not chunk then
+            break
+        end
+
+        local buffer = decoder(chunk)
+        
+        for _, speaker in pairs(speakers) do
+            while not speaker.playAudio(buffer) do
+                os.pullEvent("speaker_audio_empty")
+            end
+        end
+    end
+
+    file.close()
+    running = false
+end
+
+local function drawGUI()
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("DFPWM Player")
+    print("------------")
+    print("Files:")
+    local files = listFiles()
+    for i, file in ipairs(files) do
+        print(i .. ". " .. file)
+    end
+    print("------------")
+    print("Enter the number of the file to play, or 'stop' to stop playback:")
+end
+
+local function handleUserInput()
+    while true do
+        drawGUI()
+        local input = read()
+        if input == "stop" then
+            running = false
+            if currentPlayback then
+                currentPlayback.terminate()
+            end
+        else
+            local fileIndex = tonumber(input)
+            if fileIndex and fileIndex > 0 and fileIndex <= #listFiles() then
+                local filename = listFiles()[fileIndex]
+                if currentPlayback then
+                    currentPlayback.terminate()
+                end
+                currentPlayback = parallel.waitForAny(function() playFile(filename) end)
+            end
         end
     end
 end
 
-file.close()
-print("Playback finished")
+parallel.waitForAny(handleUserInput)
